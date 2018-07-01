1679873e46685d0f40ece59dbdfcff68b36bdc62
e5ash <e5ash.bro@gmail.com>
2018-07-01 22:21:49 +0300

    75% layout

--- src/template/scripts/common.js
+++ src/template/scripts/common.js
@@ -2,5 +2,9 @@ $(document).ready(function($) {
 
 	$('.input_phone .input__wrap').mask('+7 (000) 000-00-00');
 
-	
+	$('.reviews__list').slick({
+		slidesToShow: 3,
+		variableWidth: true,
+		centerMode: true
+	})
 });
